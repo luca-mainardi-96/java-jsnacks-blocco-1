7da76705af62206df67595981a48e423589cec8e
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-10 23:25:04 +0200

    esercizio4

--- src/Snack4.java
+++ src/Snack4.java
@@ -1,3 +1,25 @@
+import java.util.Scanner;
 public class Snack4 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        String[] guests = {"Nico Falchetti","Irene Castellani","Milo Bernardi","Aria Montesi","Enea Vassalli","Viola Sereni","Leone Carli","Mira Antinori","Noah Pellegrin","Livia Trentini"};
+
+        System.out.print("Inserisci il tuo nome: ");
+        String guestName = scan.nextLine();
+
+        Boolean invited = false;
+        for (int i=0; i<guests.length; i++){
+            if(guestName.equalsIgnoreCase(guests[i])){
+                invited = true;
+                break;
+            }
+        }
+
+        if(invited == true){
+            System.out.println("Sei invitato");
+        }else{
+            System.out.println("Non sei invitato");
+        }
+    }
     
 }
