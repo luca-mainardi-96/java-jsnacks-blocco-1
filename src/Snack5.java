32ab466bbe3bc67722bd12b172938619b3bad9ce
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-10 23:33:05 +0200

    esercizio5

--- src/Snack5.java
+++ src/Snack5.java
@@ -1,3 +1,42 @@
+import java.util.Scanner;
 public class Snack5 {
-    
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci un numero: ");
+        int firstNumber = scan.nextInt();
+        if(firstNumber%2 == 1){
+            System.out.println("Il numero è dispari");
+        }
+
+        System.out.print("Inserisci un numero: ");
+        int secondNumber = scan.nextInt();
+        if(secondNumber%2 == 1){
+            System.out.println("Il numero è dispari");
+        }
+
+        System.out.print("Inserisci un numero: ");
+        int thirdNumber = scan.nextInt();
+        if(thirdNumber%2 == 1){
+            System.out.println("Il numero è dispari");
+        }
+
+        System.out.print("Inserisci un numero: ");
+        int fourthNumber = scan.nextInt();
+        if(fourthNumber%2 == 1){
+            System.out.println("Il numero è dispari");
+        }
+
+        System.out.print("Inserisci un numero: ");
+        int fifthNumber = scan.nextInt();
+        if(fifthNumber%2 == 1){
+            System.out.println("Il numero è dispari");
+        }
+
+        System.out.print("Inserisci un numero: ");
+        int sixthNumber = scan.nextInt();
+        if(sixthNumber%2 == 1){
+            System.out.println("Il numero è dispari");
+        }
+    }
 }
