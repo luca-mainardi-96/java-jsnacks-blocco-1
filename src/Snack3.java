d630a1ff553b843452286e5739a964432a16e047
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-10 22:27:55 +0200

    esercizio 3

--- src/Snack3.java
+++ src/Snack3.java
@@ -1,3 +1,40 @@
+import java.util.Scanner;
 public class Snack3 {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci un numero: ");
+        int firstNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int secondNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int thirdNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int fourthNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int fifthNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int sixthNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int seventhNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int eighthNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int ninthNumber = scan.nextInt();
+
+        System.out.print("Inserisci un numero: ");
+        int tenthNumber = scan.nextInt();
+
+        int sum = firstNumber+secondNumber+thirdNumber+fourthNumber+fifthNumber+sixthNumber+seventhNumber+eighthNumber+ninthNumber+tenthNumber;
+        System.out.println("La somma è " + sum);
+    }
     
 }
