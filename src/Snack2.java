e34d98269db542e4ca5e87830ff3e69582af03b8
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-10 22:18:39 +0200

    esercizio2

--- src/Snack2.java
+++ src/Snack2.java
@@ -1,3 +1,20 @@
+import java.util.Scanner;
 public class Snack2 {
-    
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci la prima parola: ");
+        String firstWord = scan.next();
+
+        System.out.print("Inserisci la seconda parola: ");
+        String secondWord = scan.next();
+
+        if(firstWord.length()<secondWord.length()){
+            System.out.println(firstWord + " " + secondWord);
+        }else{
+            System.out.println(secondWord + " " + firstWord);
+        }
+        
+    }
 }
