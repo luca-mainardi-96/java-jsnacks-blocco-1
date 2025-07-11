73cf51578f2ea4ee1045d1ae3127b8ed0412f839
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-11 11:16:42 +0200

    esercizio7

--- src/Snack7.java
+++ src/Snack7.java
@@ -1,3 +1,13 @@
+import java.math.BigInteger;
+
 public class Snack7 {
-    
+    public static void main(String[] args) {
+        BigInteger base = BigInteger.valueOf(2);
+        BigInteger result = BigInteger.ONE;
+
+        for(int i = 0; i <= 1000; i++){
+            System.out.println("2^" + i + " = " + result);
+            result = result.multiply(base);
+        }
+    }
 }
