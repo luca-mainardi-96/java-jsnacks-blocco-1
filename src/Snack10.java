27eabe6c94a641d80e36ae0300aeb00db18e654e
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-11 12:04:43 +0200

    esercizio10

--- src/Snack10.java
+++ src/Snack10.java
@@ -1,3 +1,20 @@
 public class Snack10 {
-    
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+            int spaces = rows - i;
+            int asterisks = (2 * i) - 1;
+
+            for (int j = 0; j < spaces; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < asterisks; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
 }
