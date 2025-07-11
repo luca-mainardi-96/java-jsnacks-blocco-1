9a8352b697fc601e252ed22bc84a60263d033487
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-11 10:32:02 +0200

    esercizio6

--- src/Snack6.java
+++ src/Snack6.java
@@ -1,3 +1,18 @@
+import java.util.Scanner;
 public class Snack6 {
-    
+    public static void main(String[] args) {
+        
+        Scanner scan = new Scanner(System.in);
+
+        System.out.print("Inserisci un numero intero positivo: ");
+        int userNumber = scan.nextInt();
+
+        int result = 0;
+        for(int i=0; i<=userNumber; i++){
+            result = i*i*i;
+            System.out.println(result);
+        }
+
+        
+    }
 }
