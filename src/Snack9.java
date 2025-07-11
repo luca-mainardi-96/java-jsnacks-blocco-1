2bd73c818bc3f19f4c0763d3b5c54877a0c5d806
luca-mainardi-96 <luca.mainardi.12358@gmail.com>
2025-07-11 11:58:51 +0200

    esercizio9

--- src/Snack9.java
+++ src/Snack9.java
@@ -1,3 +1,14 @@
 public class Snack9 {
-    
+    public static void main(String[] args) {
+        int somma = 0;
+
+        for (int i = 1; i <= 10; i++) {
+            somma += i;
+        }
+
+        double media = somma / 10.0;
+
+        System.out.println("La somma dei primi 10 numeri è: " + somma);
+        System.out.println("La media è: " + media);
+    }
 }
